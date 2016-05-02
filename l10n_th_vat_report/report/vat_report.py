4a715794ecb97b5732a5df94fff54e1ff08163d3
Probuse <kaushik.probuse@gmail.com>
2016-05-02 12:44:00 +0530

    [FIX]l10n_th_vat_report: fixed issues with invoice tax data in report

--- l10n_th_vat_report/report/vat_report.py
+++ l10n_th_vat_report/report/vat_report.py
@@ -9,14 +9,8 @@ class VatReportParser(report_sxw.rml_parse):
         super(VatReportParser, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({
             'get_lines': self.get_lines,
-            'get_year': self.get_year,
             'get_base_total': self.get_base_total,
         })
-
-    def get_year(self, period):
-        start_date = period.date_start
-        year = datetime.datetime.strptime(start_date, '%Y-%m-%d').year
-        return year
 
     def get_base_total(self, data):
         res = self.get_lines(data)
@@ -27,12 +21,12 @@ class VatReportParser(report_sxw.rml_parse):
             tax_total += r['tax_amount']
         return {'base_total': base_total, 'tax_total': tax_total}
 
-    def get_lines(self, data):
-        period = data.period_id
-        company = data.company_id
-        tax = data.tax_id
-        base_code = data.base_code_id
-        tax_code = data.tax_code_id
+    def get_voucher_tax(self, record):
+        period = record.period_id
+        company = record.company_id
+        tax = record.tax_id
+        base_code = record.base_code_id
+        tax_code = record.tax_code_id
 
         self.cr.execute("""
             SELECT
@@ -59,8 +53,47 @@ class VatReportParser(report_sxw.rml_parse):
             GROUP BY
                 avt.id,voucher.date,voucher.number,p.name,p.vat,avt.tax_id
         """, (tax.id, base_code.id, tax_code.id, period.id, company.id))
+        voucher_tax = self.cr.dictfetchall()
+        return voucher_tax
 
-        result = self.cr.dictfetchall()
+    def get_invoice_tax(self, record):
+        period = record.period_id
+        company = record.company_id
+        base_code = record.base_code_id
+        tax_code = record.tax_code_id
+
+        self.cr.execute("""
+            SELECT
+                ait.id,
+                SUM(ait.base_amount) as base_amount,
+                SUM(ait.amount) as tax_amount,
+                invoice.date_invoice as date,
+                invoice.number as number,
+                p.name as partner_name,
+                p.vat as tax_id
+            FROM
+                account_invoice_tax as ait
+            LEFT JOIN account_invoice invoice ON
+                (ait.invoice_id = invoice.id)
+            LEFT JOIN res_partner p ON
+                (invoice.partner_id = p.id)
+            WHERE
+                ait.base_code_id = %s AND
+                ait.tax_code_id = %s AND
+                invoice.period_id = %s AND
+                ait.company_id =%s
+            GROUP BY
+                ait.id,invoice.date_invoice,invoice.number,p.name,p.vat
+        """, (base_code.id, tax_code.id, period.id, company.id))
+        invoice_tax = self.cr.dictfetchall()
+        return invoice_tax
+
+    def get_lines(self, record):
+        result = []
+        voucher_tax = self.get_voucher_tax(record)
+        result.extend(voucher_tax)
+        invoice_tax = self.get_invoice_tax(record)
+        result.extend(invoice_tax)
         return result
 
 
